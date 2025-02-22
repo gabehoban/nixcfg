e47ffdf088ff6903510207b0f6bd6874dcda3c0c
Gabe Hoban <gabehoban@icloud.com>
2025-02-21 17:58:18 -0800

    fix typo

--- hosts/workstation/realtek-r8168/default.nix
+++ hosts/workstation/realtek-r8168/default.nix
@@ -4,16 +4,3 @@
   ];
   boot.blacklistedKernelModules = ["r8169"];
 }
-
-
-{config, packages, ...}:
-{
-  imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-kde.nix>
-    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
-  ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.r8168 ];
-  boot.blacklistedKernelModules = ["r8169"];
-  hardware.enableAllFirmware = true;
-  nixpkgs.config.allowUnfree = true;
-}
