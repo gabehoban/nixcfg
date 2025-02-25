8c3c7abb20e517edab5f5be40d0fc123228e2a4f
Gabe Hoban <gabehoban@icloud.com>
2025-02-25 17:18:18 -0500

    move vscode configs to default profile

--- homes/gabehoban/vsCode/default.nix
+++ homes/gabehoban/vsCode/default.nix
@@ -8,71 +8,73 @@
     programs.vscode = {
       enable = true;
       package = pkgs.vscodium;
-      enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
+      profiles.default = {
+        enableUpdateCheck = false;
+        enableExtensionUpdateCheck = false;
 
-      keybindings = [
-        {
-          "key" = "alt+e";
-          "command" = "workbench.action.quickOpen";
-        }
-        {
-          "key" = "ctrl+p";
-          "command" = "-workbench.action.quickOpen";
-        }
-        {
-          "key" = "ctrl+p";
-          "command" = "workbench.action.showCommands";
-        }
-        {
-          "key" = "ctrl+shift+t";
-          "command" = "-workbench.action.reopenClosedEditor";
-        }
-        {
-          "key" = "ctrl+shift+t";
-          "command" = "workbench.action.tasks.runTask";
-        }
-        {
-          "key" = "ctrl+shift+p";
-          "command" = "-workbench.action.showCommands";
-        }
-      ];
+        keybindings = [
+          {
+            "key" = "alt+e";
+            "command" = "workbench.action.quickOpen";
+          }
+          {
+            "key" = "ctrl+p";
+            "command" = "-workbench.action.quickOpen";
+          }
+          {
+            "key" = "ctrl+p";
+            "command" = "workbench.action.showCommands";
+          }
+          {
+            "key" = "ctrl+shift+t";
+            "command" = "-workbench.action.reopenClosedEditor";
+          }
+          {
+            "key" = "ctrl+shift+t";
+            "command" = "workbench.action.tasks.runTask";
+          }
+          {
+            "key" = "ctrl+shift+p";
+            "command" = "-workbench.action.showCommands";
+          }
+        ];
 
-      userSettings = {
-        "diffEditor.ignoreTrimWhitespace" = false;
-        "direnv.restart.automatic" = true;
-        "editor.fontSize" = lib.mkForce 14;
-        "editor.formatOnPaste" = true;
-        "editor.formatOnSave" = true;
-        "editor.formatOnType" = true;
-        "editor.rulers" = [80];
-        "explorer.confirmDelete" = false;
-        "files.autoSave" = "afterDelay";
-        "git.autoStash" = true;
-        "git.autofetch" = true;
-        "git.confirmSync" = false;
-        "github.gitProtocol" = "ssh";
-        "window.menuBarVisibility" = "hidden";
-        "window.zoomLevel" = 1.7;
+        userSettings = {
+          "diffEditor.ignoreTrimWhitespace" = false;
+          "direnv.restart.automatic" = true;
+          "editor.fontSize" = lib.mkForce 14;
+          "editor.formatOnPaste" = true;
+          "editor.formatOnSave" = true;
+          "editor.formatOnType" = true;
+          "editor.rulers" = [80];
+          "explorer.confirmDelete" = false;
+          "files.autoSave" = "afterDelay";
+          "git.autoStash" = true;
+          "git.autofetch" = true;
+          "git.confirmSync" = false;
+          "github.gitProtocol" = "ssh";
+          "window.menuBarVisibility" = "hidden";
+          "window.zoomLevel" = 1.7;
 
-        "window.titleBarStyle" =
-          if config.myHome.desktop.gnome.enable
-          then "custom"
-          else "native";
+          "window.titleBarStyle" =
+            if config.myHome.desktop.gnome.enable
+            then "custom"
+            else "native";
+        };
+
+        extensions = with pkgs.vscode-extensions; [
+          esbenp.prettier-vscode
+          foxundermoon.shell-format
+          github.vscode-github-actions
+          github.vscode-pull-request-github
+          jnoortheen.nix-ide
+          mads-hartmann.bash-ide-vscode
+          mkhl.direnv
+          ms-python.python
+          oderwat.indent-rainbow
+          rubymaniac.vscode-paste-and-indent
+        ];
       };
-
-      extensions = with pkgs.vscode-extensions; [
-        esbenp.prettier-vscode
-        foxundermoon.shell-format
-        github.vscode-github-actions
-        github.vscode-pull-request-github
-        jnoortheen.nix-ide
-        mads-hartmann.bash-ide-vscode
-        mkhl.direnv
-        ms-python.python
-        oderwat.indent-rainbow
-        rubymaniac.vscode-paste-and-indent
-      ];
     };
   };
 }
